3a201be08189bae2aafc43ad9a9079538685ffb4
Riviere Fougy <77861258+rfougy@users.noreply.github.com>
2023-11-17 22:05:15 -0500

    Refactor: Change UI, Home Page, and Default Theme (#3)
    * refactor(Home): change copy and UI
    
    * refactor(Navbar): remove nav menu and logo from DefaultLayout
    
    * refactor: change default theme to light mode

--- components/navbar/layouts/default/DefaultLayout.tsx
+++ components/navbar/layouts/default/DefaultLayout.tsx
@@ -1,8 +1,6 @@
 import { Dispatch, SetStateAction } from "react";
 
-import NavMenu from "../../nav-menu/NavMenu";
 import ThemeToggleButton from "../../../features/theme-toggle/ThemeToggleButton";
-import NavLogo from "../../nav-logo/NavLogo";
 
 import { LogoAndButtonsBox, FeaturesBox } from "./DefaultLayout.styled";
 import { NavBox } from "../../Navbar.styled";
@@ -13,12 +11,10 @@ const DefaultLayout: React.FC<{
 }> = ({ toggleTheme, setSearchActivated }) => (
   <NavBox>
     <LogoAndButtonsBox>
-      <NavLogo />
       <FeaturesBox>
         <ThemeToggleButton toggleTheme={toggleTheme} />
       </FeaturesBox>
     </LogoAndButtonsBox>
-    <NavMenu />
   </NavBox>
 );
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,31 +10,20 @@ import {
   Margin,
 } from "../styles/pages/DisplayDotsPage.styled";
 
-const Home: NextPage = () => {
-  return (
-    <>
-      <DisplayDotsCoordsProvider>
-        <Box>
-          <Description isIntroText>
-            Hi, my name is Riviere, welcome to:
-          </Description>
-          <Margin>
-            <DisplayDotsAnime
-              text={"ITEMS.RIV.SYSTEMS"}
-              includeRestartButton={true}
-            />
-          </Margin>
-          <Description>
-            I created this website to share
-            <br />
-            my works, projects and experiences.
-          </Description>
-          <Button href={`/content`}>EXPLORE SYSTEMS</Button>
-        </Box>
-      </DisplayDotsCoordsProvider>
-    </>
-  );
-};
+const Home: NextPage = () => (
+  <>
+    <DisplayDotsCoordsProvider>
+      <Box>
+        <Description isIntroText>Sorry, this site is currently...</Description>
+        <Margin>
+          <DisplayDotsAnime text={"IN PROGRESS."} />
+        </Margin>
+        <Description>Come back again soon!</Description>
+        <Button href="https://riv.systems/">TO RIV.SYSTEMS</Button>
+      </Box>
+    </DisplayDotsCoordsProvider>
+  </>
+);
 
 export default Home;
 
