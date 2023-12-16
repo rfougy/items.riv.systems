ca0e09bffe122cf374627efaf43fd0fb52715428
Riviere Fougy <rivierefougy@gmail.com>
2023-12-15 21:20:31 -0500

    style(GridView): minimize lines of code

--- components/results/views/grid/GridView.tsx
+++ components/results/views/grid/GridView.tsx
@@ -4,22 +4,20 @@ import { Grid, GridItem } from "./GridView.styled";
 
 const GridView: React.FC<{
   content: any;
-}> = ({ content }) => {
-  return content ? (
+}> = ({ content }) =>
+  content ? (
     <Grid>
-      {content.map((singleContent: any, index: number) => {
-        const { path, frontmatter } = singleContent;
-
-        return (
-          <GridItem key={index}>
-            <PostCard path={path} frontmatter={frontmatter} />
-          </GridItem>
-        );
-      })}
+      {content.map((singleContent: any, index: number) => (
+        <GridItem key={index}>
+          <PostCard
+            path={singleContent.path}
+            frontmatter={singleContent.frontmatter}
+          />
+        </GridItem>
+      ))}
     </Grid>
   ) : (
     <div>Error: No content available...</div>
   );
-};
 
 export default GridView;
