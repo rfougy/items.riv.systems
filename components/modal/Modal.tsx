3cfcde163b883a09df72cb73fc154f9089cf1e17
Riviere Fougy <rivierefougy@gmail.com>
2023-12-16 14:00:46 -0500

    feat: useOutslideClick hook to close modal

--- components/modal/Modal.tsx
+++ components/modal/Modal.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useRef } from "react";
 import { useModalContext } from "../../context/ModalContext";
 import {
   Content,
@@ -7,6 +8,7 @@ import {
   Text,
   ViewportWrapper,
 } from "./Modal.styled";
+import useOutsideClick from "../../hooks/useOutsideClick";
 
 const Modal: React.FC = () => {
   const {
@@ -17,13 +19,23 @@ const Modal: React.FC = () => {
     handlePrevSlide,
   } = useModalContext();
 
+  const contentRef = useRef(null);
+  const nextButtonRef = useRef(null);
+  const prevButtonRef = useRef(null);
+
+  useOutsideClick(contentRef, () => handleModalClose());
+  useOutsideClick(nextButtonRef, () => handleModalClose());
+  useOutsideClick(prevButtonRef, () => handleModalClose());
+
   return (
     openModal && (
       <ViewportWrapper>
         <button onClick={() => handleModalClose()}>CLOSE</button>
         <ModalContainer>
-          <button onClick={() => handlePrevSlide()}>PREV</button>
-          <Content>
+          <button ref={prevButtonRef} onClick={() => handlePrevSlide()}>
+            PREV
+          </button>
+          <Content ref={contentRef}>
             <ImageBox>
               <Image
                 src={currSlide.frontmatter.coverImage}
@@ -39,7 +51,9 @@ const Modal: React.FC = () => {
             </ImageBox>
             <Text>{currSlide.frontmatter.excerpt}</Text>
           </Content>
-          <button onClick={() => handleNextSlide()}>NEXT</button>
+          <button ref={nextButtonRef} onClick={() => handleNextSlide()}>
+            NEXT
+          </button>
         </ModalContainer>
         <div />
       </ViewportWrapper>
--- hooks/useOutsideClick.ts
+++ hooks/useOutsideClick.ts
@@ -1,26 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, RefObject } from "react";
 
-function useOutsideClick(ref: any, callback: () => any) {
-  const [clickedOutside, setClickedOutside] = useState<boolean>(false);
-
-  function handleOutsideClick(event: MouseEvent) {
-    if (ref.current && !ref.current.contains(event.target as Node)) {
-      setClickedOutside(true);
-      callback();
-    } else {
-      setClickedOutside(false);
-    }
-  }
-
+function useOutsideClick(ref: RefObject<any>, callback: () => any): void {
   useEffect(() => {
-    document.addEventListener("mousedown", handleOutsideClick);
+    function handleOutsideClick(event: MouseEvent): void {
+      if (ref.current && !ref.current.contains(event.target as Node))
+        callback();
+    }
 
+    window.addEventListener("mousedown", handleOutsideClick);
     return () => {
-      document.removeEventListener("mousedown", handleOutsideClick);
+      window.removeEventListener("mousedown", handleOutsideClick);
     };
-  }, []);
-
-  return clickedOutside;
+  }, [ref, callback]);
 }
 
 export default useOutsideClick;
